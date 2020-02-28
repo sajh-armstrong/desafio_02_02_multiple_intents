dffadb581e6d7ea53209e703a4dc6f034316eb2d
Sajh Armstrong <sajharmstrong@gmail.com>
2020-02-28 16:52:20 -0300

    first commit

---
+++ app/src/main/java/com/desafiolatam/desafio_02_02_multiples_intents/MainActivity.java
@@ -0,0 +1,16 @@
+package com.desafiolatam.desafio_02_02_multiples_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //TODO item 2
+}
